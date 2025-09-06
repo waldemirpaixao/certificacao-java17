34e4f9b1e2fcf668ac0b3106317a4d1de8631dc3
Waldemir Santos Silva Paixão <waldemir.ufs@gmail.com>
2025-09-06 06:59:32 -0300

    StringBuilder

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Olá sejam tod@s muito bem-vindos a Certificação Java 17
+
+Esta está sendo a minha trajetória no processo de Programação Java, fazendo um hands-on dos códigos e entendendo o comportamento dos mesmos.
+
+Desejo sucesso para todos!
+
+Segue abaixo a lista de músicas usadas para o processo de certificação, para deixar a caminhada mais harmoniosa, leve e empolgante.
+
+**Música para programar:**  
+[Music for Work — Deep Focus Mix para programação e codificação](https://www.youtube.com/watch?v=mhNg55_IYiw)
---
+++ src/main/java/br/dev/certificacao/chapter4_core_apis/Creating_The_StringBuilder_Class.java
@@ -0,0 +1,130 @@
+package br.dev.certificacao.chapter4_core_apis;
+
+public class Creating_The_StringBuilder_Class {
+
+    public Creating_The_StringBuilder_Class() {
+
+        System.out.println("Creating_The_StringBuilder_Class");
+        System.out.println("----------------------------------");
+        System.out.println("This is s simple example of StringBuilder class");
+
+        System.out.println("""
+                This is the example code:
+                             StringBuilder alpha = new StringBuilder();
+
+                    for (char current = 'a'; current <= 'z'; current++)
+                        alpha.append(current);
+                    System.out.println(alpha);
+                            """);
+
+        StringBuilder alpha = new StringBuilder();
+
+        for (char current = 'a'; current <= 'z'; current++)
+            alpha.append(current);
+        System.out.println(alpha);
+
+    }
+
+    public static void main(String[] args) {
+        Creating_The_StringBuilder_Class creating_The_StringBuilder_Class = new Creating_The_StringBuilder_Class();
+        creating_The_StringBuilder_Class.multability_an_chaining();
+        creating_The_StringBuilder_Class.creating_a_stringBuilder();
+        creating_The_StringBuilder_Class.important_stringBuilder_methods();
+    }
+
+    private void multability_an_chaining() {
+
+        System.out.println("");
+
+        System.out.println("Multability and Chaining");
+        System.out.println("------------------------");
+
+        System.out.println("Multability");
+        System.out.println("-------------------------------------");
+        System.out.println("Take a look the code below:");
+        System.out.println("""
+
+        StringBuilder sb = new StringBuilder("start");
+        sb.append("+middle");
+        System.out.println("Printing the sb result " + sb);
+
+        StringBuilder same = sb.append("+end");
+        System.out.println("Printing the same result " + same);
+
+        System.out.println("Verifying is is the same object");
+        System.out.println(same == sb);
+
+        System.out.println(sb.equals(same));
+                """);
+
+        StringBuilder sb = new StringBuilder("start");
+        sb.append("+middle");
+        System.out.println("Printing the sb result " + sb);
+
+        StringBuilder same = sb.append("+end");
+        System.out.println("Printing the same result " + same);
+
+        System.out.println("Verifying is is the same object");
+        System.out.println(same == sb);
+
+        System.out.println(sb.equals(same));
+
+
+        System.out.println("-------------------------------------");
+        System.out.println("Chaining");
+        System.out.println("-------------------------------------");  
+        
+        System.out.println("Take a look the code below:");
+        System.out.println("""
+
+        StringBuilder a  =  new StringBuilder("abc");
+        StringBuilder b = a.append("de");
+        b = b.append("f").append("g");
+        System.out.println("Verifying the value of a and b");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+                
+                """);
+        
+        StringBuilder a  =  new StringBuilder("abc");
+        StringBuilder b = a.append("de");
+        b = b.append("f").append("g");
+        System.out.println("Verifying the value of a and b");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+
+    }
+
+    private void creating_a_stringBuilder(){
+
+        System.out.println("");
+
+        System.out.println("Creating a StringBuilder");
+        System.out.println("------------------------");
+
+        System.out.println("Take a look the code below:");
+          System.out.println("");
+
+        System.out.println("""
+        StringBuilder sb1 = new StringBuilder();-> this one creates an empty StringBuilder
+        StringBuilder sb2 = new StringBuilder("Waldemir");-> this one creates a StringBuilder with the value passed as parameter
+        Note: the first two StringBuilder above tell java to manage the implementation details
+        StringBuilder sb3 = new StringBuilder(10);-> the last one creates a StringBuilder with an initial capacity passed as parameter
+                """);
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder("Waldemir");
+        StringBuilder sb3 = new StringBuilder(10);
+
+     
+    }
+
+    private void important_stringBuilder_methods(){
+
+        
+
+    }
+}
