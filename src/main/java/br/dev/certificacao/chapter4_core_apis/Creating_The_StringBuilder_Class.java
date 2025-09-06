5b8ac1d509d4b2cf06563b66853baea0729d8f1f
Waldemir Santos Silva Paixão <waldemir.ufs@gmail.com>
2025-09-06 07:25:04 -0300

    StringBuilder - using_commom_methods

--- src/main/java/br/dev/certificacao/chapter4_core_apis/Creating_The_StringBuilder_Class.java
+++ src/main/java/br/dev/certificacao/chapter4_core_apis/Creating_The_StringBuilder_Class.java
@@ -44,6 +44,19 @@ public class Creating_The_StringBuilder_Class {
         System.out.println("Take a look the code below:");
         System.out.println("""
 
+                StringBuilder sb = new StringBuilder("start");
+                sb.append("+middle");
+                System.out.println("Printing the sb result " + sb);
+
+                StringBuilder same = sb.append("+end");
+                System.out.println("Printing the same result " + same);
+
+                System.out.println("Verifying is is the same object");
+                System.out.println(same == sb);
+
+                System.out.println(sb.equals(same));
+                        """);
+
         StringBuilder sb = new StringBuilder("start");
         sb.append("+middle");
         System.out.println("Printing the sb result " + sb);
@@ -55,50 +68,33 @@ public class Creating_The_StringBuilder_Class {
         System.out.println(same == sb);
 
         System.out.println(sb.equals(same));
-                """);
-
-        StringBuilder sb = new StringBuilder("start");
-        sb.append("+middle");
-        System.out.println("Printing the sb result " + sb);
-
-        StringBuilder same = sb.append("+end");
-        System.out.println("Printing the same result " + same);
-
-        System.out.println("Verifying is is the same object");
-        System.out.println(same == sb);
-
-        System.out.println(sb.equals(same));
-
 
         System.out.println("-------------------------------------");
         System.out.println("Chaining");
-        System.out.println("-------------------------------------");  
-        
+        System.out.println("-------------------------------------");
+
         System.out.println("Take a look the code below:");
         System.out.println("""
 
-        StringBuilder a  =  new StringBuilder("abc");
+                StringBuilder a  =  new StringBuilder("abc");
+                StringBuilder b = a.append("de");
+                b = b.append("f").append("g");
+                System.out.println("Verifying the value of a and b");
+                System.out.println("a = " + a);
+                System.out.println("b = " + b);
+
+                        """);
+
+        StringBuilder a = new StringBuilder("abc");
         StringBuilder b = a.append("de");
         b = b.append("f").append("g");
         System.out.println("Verifying the value of a and b");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-                
-                """);
-        
-        StringBuilder a  =  new StringBuilder("abc");
-        StringBuilder b = a.append("de");
-        b = b.append("f").append("g");
-        System.out.println("Verifying the value of a and b");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-
-
-
 
     }
 
-    private void creating_a_stringBuilder(){
+    private void creating_a_stringBuilder() {
 
         System.out.println("");
 
@@ -106,25 +102,45 @@ public class Creating_The_StringBuilder_Class {
         System.out.println("------------------------");
 
         System.out.println("Take a look the code below:");
-          System.out.println("");
+        System.out.println("");
 
-        System.out.println("""
-        StringBuilder sb1 = new StringBuilder();-> this one creates an empty StringBuilder
-        StringBuilder sb2 = new StringBuilder("Waldemir");-> this one creates a StringBuilder with the value passed as parameter
-        Note: the first two StringBuilder above tell java to manage the implementation details
-        StringBuilder sb3 = new StringBuilder(10);-> the last one creates a StringBuilder with an initial capacity passed as parameter
-                """);
+        System.out.println(
+                """
+                        StringBuilder sb1 = new StringBuilder();-> this one creates an empty StringBuilder
+                        StringBuilder sb2 = new StringBuilder("Waldemir");-> this one creates a StringBuilder with the value passed as parameter
+                        Note: the first two StringBuilder above tell java to manage the implementation details
+                        StringBuilder sb3 = new StringBuilder(10);-> the last one creates a StringBuilder with an initial capacity passed as parameter
+                                """);
 
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder("Waldemir");
         StringBuilder sb3 = new StringBuilder(10);
 
-     
     }
 
-    private void important_stringBuilder_methods(){
+    private void important_stringBuilder_methods() {
 
+        using_commom_methods();
+
+    }
+
+    private void using_commom_methods() {
+
+        System.out.println("");
+
+        System.out.println("Important StringBuilder Methods");
+        System.out.println("-------------------------------");
+
+        System.out.println("Take a look the code below:");
+
+        System.out.println("Take a look the result below:");
+
+        var sb = new StringBuilder("Waldemir");
+        System.out.println("Tamanho: " + sb.length());
+        System.out.println("SubString(0,1): " + sb.substring(0, 1));
+        System.out.println("Substring(indexOf(wa),indexOf(de): " + sb.substring(sb.indexOf("W"), sb.indexOf("de")));
+        System.out.println("CharAt(0): " + sb.charAt(0));
         
-
     }
+
 }
